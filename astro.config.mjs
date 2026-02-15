35b7239b61f4069f9916c767a60783fba3afc201
lancelot <lancelot@lancelots-MacBook-Air.local>
2026-02-15 15:24:53 +0800

    added new things

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,17 @@ import icon from 'astro-icon';
 
 import preact from '@astrojs/preact';
 
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://hongyicai.cc',
-    integrations: [mdx(), sitemap(), icon(), preact()],
+    integrations: [mdx(
+        {
+            remarkPlugins: [remarkMath],
+            rehypePlugins: [rehypeKatex],
+        }
+    ), sitemap(), icon(), preact()],
+
 });
